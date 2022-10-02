385bf2d9f8eec03921f55153dab19605526b0b55
ankur-srivastava <ankurlkw@gmail.com>
2022-10-02 22:39:14 +0530

    first draft

---
+++ server.js
@@ -0,0 +1,18 @@
+import { ApolloServer, gql } from 'apollo-server'
+
+const typeDefs = gql`
+    type Query {
+        greeting: String
+    }
+`
+
+const resolvers = {
+    Query: {
+        greeting: () => 'Hello World'
+    }
+}
+
+const server = new ApolloServer({typeDefs, resolvers})
+
+const serverInfo = await server.listen({port: 9000})
+console.log('Server running at ', serverInfo.url)
